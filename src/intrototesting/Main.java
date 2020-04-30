102c4e7f35765a56e885288ddd09abdd60e95316
Carlos de Tena Bellmont <carlostena@centroafuera.es>
2020-04-30 17:52:35 +0200

    Primer Commit

---
+++ src/intrototesting/Main.java
@@ -0,0 +1,22 @@
+package intrototesting;
+
+import java.util.Scanner;
+
+public class Main {
+
+	private static final int NUM_REPETICIONES = 3;
+	
+	public static void main(String[] args) {
+		Suma suma = new Suma();
+		Scanner teclado = new Scanner(System.in);
+
+		for (int i = 0; i < NUM_REPETICIONES; i++) {
+			suma.obtenerValores(teclado);
+		}
+        teclado.close();
+	}
+	
+	
+
+
+}
