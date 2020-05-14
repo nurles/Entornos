641a52f37f192c43e2b5fcb5a3a1db6a70718e3c
Nuria <nuria.lester@gmail.com>
2020-05-14 19:46:15 +0200

    Primera potencia

--- src/intrototesting/Potencia.java
+++ src/intrototesting/Potencia.java
@@ -3,7 +3,8 @@ package intrototesting;
 public class Potencia {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		System.out.println("Esto es una potencia.")
 
 	}
 
